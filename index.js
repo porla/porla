200d942b702fd4d5769c748ea33ed0ea2edfcbfe
Viktor Elofsson <viktor@viktorelofsson.se>
2019-01-28 07:54:50 +0100

    Refactoring session and Porla in general

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-const lt = require('@porla/libtorrent');
-const Plugin = require('./lib/plugin');
-const Porla = require('./lib/porla');
-
-module.exports = {
-    // Re-export @porla/libtorrent in case we need it from plugins. Ideally,
-    // this should not be needed as Porla should provide the APIs - but you
-    // never know ¯\_(ツ)_/¯
-    lt: lt,
-
-    Plugin: Plugin,
-    Porla: Porla
-};
+const porla = require('./lib/porla');
+module.exports = porla;
