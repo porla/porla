be6d1bc91c69462fbb225c86d2f823df958ec5d0
Viktor Elofsson <viktor@viktorelofsson.se>
2022-11-18 19:30:40 +0100

    Generate web UI binary source file

--- cmake/FileEmbed.cmake
+++ cmake/FileEmbed.cmake
@@ -1,99 +1,40 @@
-function(FileEmbedSetup)
+message("Checking ${CMAKE_SOURCE_DIR}/webui.zip")
 
-    if (NOT EXISTS ${CMAKE_BINARY_DIR}/file_embed)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}file_embed)
-    endif ()
+if (EXISTS "${CMAKE_SOURCE_DIR}/webui.zip")
+    message("Embedding ${CMAKE_SOURCE_DIR}/webui.zip in Porla")
 
-    if (NOT EXISTS ${CMAKE_BINARY_DIR}/file_embed/file_embed_empty.c)
-        file(WRITE ${CMAKE_BINARY_DIR}/file_embed/file_embed_empty.c "")
-    endif ()
+    file(READ "${CMAKE_SOURCE_DIR}/webui.zip" content HEX)
 
-    add_library(file_embed ${CMAKE_BINARY_DIR}/file_embed/file_embed_empty.c)
-    target_include_directories(file_embed PUBLIC ${CMAKE_BINARY_DIR}/file_embed)
+    # Separate into individual bytes.
+    string(REGEX MATCHALL "([A-Fa-f0-9][A-Fa-f0-9])" SEPARATED_HEX ${content})
 
-endfunction()
-
-function(FileEmbedAdd file)
-    FileEmbedGenerate(${file} var)
-    target_sources(file_embed PUBLIC ${var})
-
-    if (EXISTS "${file}")
-        add_custom_command(
-            OUTPUT ${var}
-            COMMAND ${CMAKE_COMMAND}
-            -DRUN_FILE_EMBED_GENERATE=1
-            -DFILE_EMBED_GENERATE_PATH=${file}
-            -P ${CMAKE_SOURCE_DIR}/cmake/FileEmbed.cmake
-            MAIN_DEPENDENCY ${file})
-    endif()
-endfunction()
-
-function(FileEmbedGenerate file generated_c)
-    get_filename_component(base_filename ${file} NAME)
-    set(output_filename "${base_filename}.c")
-    string(MAKE_C_IDENTIFIER ${base_filename} c_name)
-
-    set(output_c "")
-
-    if (EXISTS "${file}")
-        file(READ ${file} content HEX)
-
-        # Separate into individual bytes.
-        string(REGEX MATCHALL "([A-Fa-f0-9][A-Fa-f0-9])" SEPARATED_HEX ${content})
-
-        set(counter 0)
-        foreach (hex IN LISTS SEPARATED_HEX)
-            string(APPEND output_c "0x${hex},")
-            MATH(EXPR counter "${counter}+1")
-            if (counter GREATER 16)
-                string(APPEND output_c "\n    ")
-                set(counter 0)
-            endif ()
-        endforeach ()
-    endif()
-        set(output_c "
-    #include \"${c_name}.h\"
-    uint8_t ${c_name}_data[] = {
-        ${output_c}
-    }\;
-    unsigned ${c_name}_size = sizeof(${c_name}_data)\;
-    ")
-
-        set(output_h "
-    #ifndef ${c_name}_H
-    #define ${c_name}_H
-    #include \"stdint.h\"
-    extern uint8_t ${c_name}_data[]\;
-    extern unsigned ${c_name}_size\;
-    #endif // ${c_name}_H
-        ")
-
-    if (NOT EXISTS ${CMAKE_BINARY_DIR}/file_embed)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}file_embed)
-    endif ()
-
-
-    file(WRITE ${CMAKE_BINARY_DIR}/file_embed/${c_name}.c
-            ${output_c})
-
-    file(WRITE ${CMAKE_BINARY_DIR}/file_embed/${c_name}.h
-            ${output_h})
-
-    set(${generated_c} ${CMAKE_BINARY_DIR}/file_embed/${c_name}.c PARENT_SCOPE)
-
-endfunction()
+    set(counter 0)
+    foreach (hex IN LISTS SEPARATED_HEX)
+        string(APPEND output_c "0x${hex},")
+        MATH(EXPR counter "${counter}+1")
+        if (counter GREATER 16)
+            string(APPEND output_c "\n    ")
+            set(counter 0)
+        endif ()
+    endforeach ()
+endif()
 
 set(WEBUI_SRC "
 #include \"stddef.h\"
 #include \"stdint.h\"
+
+const uint8_t data[] = {
+    ${output_c}
+};
+
 extern \"C\" const uint8_t* webui_zip_data()
 {
-    return nullptr;
+    return data;
 }
 
 extern \"C\" const size_t webui_zip_size()
 {
-    return 0;
+    return sizeof(data);
 }")
 
 if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/webui.cpp)
