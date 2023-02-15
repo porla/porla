375b567d911d7e6dd1198a94d4f39984777e963e
Viktor Elofsson <viktor@viktorelofsson.se>
2023-02-15 11:25:56 +0100

    Add support for args, env, working dir

--- src/workflows/actions/exec.cpp
+++ src/workflows/actions/exec.cpp
@@ -8,12 +8,40 @@ namespace bp = boost::process;
 using porla::Workflows::ActionCallback;
 using porla::Workflows::Actions::Exec;
 
+struct ExecInput
+{
+    std::string                                       file{};
+    std::optional<std::vector<std::string>>           args{};
+    std::optional<std::map<std::string, std::string>> env{};
+    std::optional<std::string>                        working_dir{};
+};
+
 struct Exec::ExecState
 {
     bp::child c;
     std::future<std::string> std_err;
     std::future<std::string> std_out;
 };
+
+static void from_json(const nlohmann::json& j, ExecInput& ei)
+{
+    j["file"].get_to(ei.file);
+
+    if (j.contains("args"))
+    {
+        ei.args = j["args"].get<std::vector<std::string>>();
+    }
+
+    if (j.contains("env"))
+    {
+        ei.env = j["env"].get<std::map<std::string, std::string>>();
+    }
+
+    if (j.contains("working_dir"))
+    {
+        ei.working_dir = j["working_dir"].get<std::string>();
+    }
+}
 
 Exec::Exec(boost::asio::io_context& io)
     : m_io(io)
@@ -24,11 +52,25 @@ Exec::~Exec() = default;
 
 void Exec::Invoke(const ActionParams& params, std::shared_ptr<ActionCallback> callback)
 {
+    bp::environment env = boost::this_process::environment();
+
+    const ExecInput input = params.Input();
+
+    if (const auto input_env = input.env)
+    {
+        for (const auto &[key, val]: *input_env)
+        {
+            env[key] = val;
+        }
+    }
+
     auto state = std::make_shared<ExecState>();
     state->c = bp::child(
         m_io,
-        params.Input()["file"].get<std::string>(),
-        params.Input()["args"].get<std::string>(),
+        input.file,
+        bp::args(input.args.value_or(std::vector<std::string>())),
+        bp::env(env),
+        bp::start_dir(input.working_dir.value_or("")),
         bp::std_err > state->std_err,
         bp::std_out > state->std_out,
         bp::on_exit(
