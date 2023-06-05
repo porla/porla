0ffa233eeba7504ea1f62f0bac11066d7c3a7332
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-06-05 23:24:22 +0300

    build-env: Initial arm64 support, part two

--- builder-env.sh
+++ builder-env.sh
@@ -6,7 +6,7 @@ if [[ $architecture == "x86_64" ]]; then
     cmake_options="-DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-linux-musl-release"
 elif [[ $architecture == "aarch64" ]]; then
     binary_url="https://github.com/userdocs/ninja-build-static/releases/download/v1.11.1/ninja-arm64v8"
-    cmake_options='-DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=aarch64-linux-musl-release'
+    cmake_options='-DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=arm64-linux-musl-release'
 else
    echo "Unsupported architecture: $architecture"
    exit 1
@@ -18,6 +18,6 @@ echo "Binary downloaded successfully"
 mv ninja /usr/bin
 chmod +x /usr/bin/ninja
 
-echo "Building binary for $architecture..."
+echo "Building for $architecture..."
 mkdir build && cd build
 cmake -DCMAKE_EXE_LINKER_FLAGS="-static -Os" $cmake_options -G Ninja ..
