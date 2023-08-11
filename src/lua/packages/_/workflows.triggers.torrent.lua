90b99163ef34608abf98e9784f9abadd91629392
Viktor Elofsson <viktor@viktorelofsson.se>
2023-08-11 09:04:16 +0200

    Add missing torrent workflow triggers

--- src/lua/packages/_/workflows.triggers.torrent.lua
+++ src/lua/packages/_/workflows.triggers.torrent.lua
@@ -32,6 +32,36 @@ return {
 
             table.insert(signals, signal)
         end
+    end,
+
+    paused = function()
+        return function(callback)
+            local signal = events.on("torrent_paused", function(torrent)
+                callback(torrent)
+            end)
+
+            table.insert(signals, signal)
+        end
+    end,
+
+    removed = function()
+        return function(callback)
+            local signal = events.on("torrent_removed", function(info_hash)
+                callback({ info_hash = info_hash })
+            end)
+
+            table.insert(signals, signal)
+        end
+    end,
+
+    resumed = function()
+        return function(callback)
+            local signal = events.on("torrent_resumed", function(torrent)
+                callback(torrent)
+            end)
+
+            table.insert(signals, signal)
+        end
     end
 }
 
