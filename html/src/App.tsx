80c95a2ef3270e561a13c1ecfbc8b36dccdeb0b4
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-12-10 21:58:28 +0100

    add trackers list linting

--- html/src/App.tsx
+++ html/src/App.tsx
@@ -1,5 +1,5 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import {Fragment, useState} from 'react'
+import {Dialog, Transition} from '@headlessui/react'
 import {
   Bars3Icon,
   ChevronLeftIcon,
@@ -11,23 +11,57 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 
+import PorlaLogo from "./porla.svg?react";
+
 import ProgressBar from "./components/ProgressBar.tsx";
 
-const teams = []
 
 const navigation = [
 
-  { name: 'All', href: '#', icon: CloudIcon, current: true, count: 122 },
-  { name: 'Downloading', href: '#', icon: CloudArrowDownIcon, current: false, count: 33 },
-  { name: 'Seeding', href: '#', icon: CloudArrowUpIcon, current: false, count: 89 },
-  { name: 'Error', href: '#', icon: ExclamationTriangleIcon, current: false, count: 0 },
+  {name: 'All', href: '#', icon: CloudIcon, current: true, count: 122},
+  {name: 'Downloading', href: '#', icon: CloudArrowDownIcon, current: false, count: 33},
+  {name: 'Seeding', href: '#', icon: CloudArrowUpIcon, current: false, count: 89},
+  {name: 'Error', href: '#', icon: ExclamationTriangleIcon, current: false, count: 0},
+
+]
+
+const trackers = [
+
+  {url: "https://tracker1.porla.org/announce/s0m3p455k3y", href: '#', icon: PorlaLogo, current: true, count: 2},
+  {url: "https://tracker2.porla.org/announce/4n0th3rp455k3y", href: '#', icon: PorlaLogo, current: false, count: 0}
 
 ]
 
 const torrents = [
 
-  { id: 1, name: 'debian-12.2.0-amd64-netinst.iso', size: '287.4 MiB', progress: 80, status: 'Downloading', download_speed: '117.00 MiB/s', upload_speed: '117.00 MiB/s', active_seeds: 1, inactive_seeds: 2, active_peers: 3, inactive_peers: 4, ratio: 13.37 },
-  { id: 2, name: "archlinux-2023.12.01-x86_64.iso", size: "872 MiB", progress: 100, status: 'Seeding', download_speed: '1.17 GiB/s', upload_speed: '1.17 GiB/s', active_seeds: 5, inactive_seeds: 6, active_peers: 7, inactive_peers: 8, ratio: 133.7  }
+  {
+    id: 1,
+    name: 'debian-12.2.0-amd64-netinst.iso',
+    size: '287.4 MiB',
+    progress: 80,
+    status: 'Downloading',
+    download_speed: '117.00 MiB/s',
+    upload_speed: '117.00 MiB/s',
+    active_seeds: 1,
+    inactive_seeds: 2,
+    active_peers: 3,
+    inactive_peers: 4,
+    ratio: 13.37
+  },
+  {
+    id: 2,
+    name: "archlinux-2023.12.01-x86_64.iso",
+    size: "872 MiB",
+    progress: 100,
+    status: 'Seeding',
+    download_speed: '1.17 GiB/s',
+    upload_speed: '1.17 GiB/s',
+    active_seeds: 5,
+    inactive_seeds: 6,
+    active_peers: 7,
+    inactive_peers: 8,
+    ratio: 133.7
+  }
   // More torrents...
 
 ]
@@ -66,7 +100,8 @@ function App() {
         {/* Static sidebar for desktop */}
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 dark:border-neutral-600 bg-white dark:bg-neutral-800 px-6">
+          <div
+            className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 dark:border-neutral-600 bg-white dark:bg-neutral-800 px-6">
             <div className="flex h-16 shrink-0 items-center">
               <img
                 className="h-8 w-auto"
@@ -121,28 +156,42 @@ function App() {
                 <li>
                   <div className="text-xs font-semibold leading-6 text-gray-400">Trackers</div>
                   <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
+                    {trackers.map((tracker) => (
+                      <li key={tracker.url}>
                         <a
-                          href={team.href}
+                          href={tracker.href}
                           className={classNames(
-                            team.current
-                              ? 'bg-gray-50 text-indigo-600'
-                              : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
+                            tracker.current
+                              ? 'bg-gray-50 dark:bg-neutral-600 text-neutral-600 dark:text-neutral-200'
+                              : 'text-neutral-700 dark:text-neutral-500 hover:text-neutral-600 hover:dark:text-neutral-200 hover:bg-neutral-50 hover:dark:bg-neutral-700',
                             'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                           )}
                         >
-                          <span
+                          <tracker.icon
                             className={classNames(
-                              team.current
-                                ? 'text-indigo-600 border-indigo-600'
-                                : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600',
-                              'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'
+                              tracker.current
+                                ? 'text-neutral-600 dark:text-neutral-200'
+                                : 'text-gray-400 dark:text-neutral-400 group-hover:text-neutral-600 group-hover:dark:text-neutral-200',
+                              'h-6 w-6 shrink-0'
                             )}
-                          >
-                            {team.initial}
-                          </span>
-                          <span className="truncate">{team.name}</span>
+                            aria-hidden="true"
+                          />
+                          <div className="truncate">
+                            {(new URL(tracker.url)).hostname}
+                          </div>
+                          {tracker.count >= 0 ? (
+                            <span
+                              className={classNames(
+                                tracker.current
+                                  ? 'dark:ring-neutral-400'
+                                  : 'group-hover:dark:ring-neutral-500 dark:ring-neutral-600',
+                                'ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-white dark:bg-neutral-800 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-600 dark:text-neutral-400 ring-1 ring-inset ring-gray-200'
+                              )}
+                              aria-hidden="true"
+                            >
+                              {tracker.count}
+                            </span>
+                          ) : null}
                         </a>
                       </li>
                     ))}
@@ -167,10 +216,11 @@ function App() {
           </div>
         </div>
 
-        <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white dark:bg-neutral-800 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+        <div
+          className="sticky top-0 z-40 flex items-center gap-x-6 bg-white dark:bg-neutral-800 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
           <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
             <span className="sr-only">Open sidebar</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            <Bars3Icon className="h-6 w-6" aria-hidden="true"/>
           </button>
           <div className="flex-1 text-sm font-semibold leading-6 text-neutral-300">Dashboard</div>
           <a href="#">
@@ -185,40 +235,40 @@ function App() {
 
         <main className="py-0 sm:py-10 lg:pl-72">
           <div className="px-4 sm:px-6 lg:px-8 overflow-auto">
-          <table className="min-w-full divide-y divide-gray-700">
-                    <thead>
-                    <tr className="text-sm font-semibold text-neutral-300">
-                      <th scope="col" className="px-3 sm:px-0 py-3.5 text-left">
-                        Name
-                      </th>
-                      <th scope="col" className="px-3 py-3.5 text-right">
-                        Size
-                      </th>
-                      <th scope="col" className="px-3 py-3.5 text-center">
-                        Progress
-                      </th>
-                      <th scope="col" className="px-3 py-3.5 text-left">
-                        Status
-                      </th>
-                      <th scope="col" className="px-3 py-3.5 text-left">
-                        Download Speed
-                      </th>
-                      <th scope="col" className="px-3 py-3.5 text-left">
-                        Upload Speed
-                      </th>
-                      <th scope="col" className="px-3 py-3.5 text-left">
-                        Seeds
-                      </th>
-                      <th scope="col" className="px-3 py-3.5 text-left">
-                        Peers
-                      </th>
-                      <th scope="col" className="px-3 py-3.5 text-left">
-                        Ratio
-                      </th>
-                    </tr>
-                    </thead>
-            <tbody className="divide-y divide-gray-800">
-            {torrents.map((torrent) => (
+            <table className="min-w-full divide-y divide-gray-700">
+              <thead>
+              <tr className="text-sm font-semibold text-neutral-300">
+                <th scope="col" className="px-3 sm:px-0 py-3.5 text-left">
+                  Name
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-right">
+                  Size
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-center">
+                  Progress
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left">
+                  Status
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left">
+                  Download Speed
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left">
+                  Upload Speed
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left">
+                  Seeds
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left">
+                  Peers
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left">
+                  Ratio
+                </th>
+              </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-800">
+              {torrents.map((torrent) => (
                 <tr key={torrent.id} className="whitespace-nowrap text-sm text-neutral-300">
                   <td className="px-3 py-4 sm:pl-0">
                     {torrent.name}
@@ -236,9 +286,9 @@ function App() {
                     <span className={`${getColor(torrent.ratio)}`}>{torrent.ratio}</span>
                   </td>
                 </tr>
-            ))}
-            </tbody>
-          </table>
+              ))}
+              </tbody>
+            </table>
           </div>
         </main>
       </div>
