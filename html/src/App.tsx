c0e37a14f50f7da4010fa74fd744e5bf0f6d8a5b
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-12-11 12:14:47 +0100

    gotta learn how to lint

--- html/src/App.tsx
+++ html/src/App.tsx
@@ -1,5 +1,5 @@
-import {Fragment, useState} from 'react'
-import {Dialog, Transition} from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import {
   Bars3Icon,
   ChevronLeftIcon,
@@ -19,17 +19,17 @@ import { CollapsibleSection } from "./components/CollapsibleSection.tsx";
 
 const navigation = [
 
-  {name: 'All', href: '#', icon: CloudIcon, current: true, count: 122},
-  {name: 'Downloading', href: '#', icon: CloudArrowDownIcon, current: false, count: 33},
-  {name: 'Seeding', href: '#', icon: CloudArrowUpIcon, current: false, count: 89},
-  {name: 'Error', href: '#', icon: ExclamationTriangleIcon, current: false, count: 0},
+  { name: 'All', href: '#', icon: CloudIcon, current: true, count: 122 },
+  { name: 'Downloading', href: '#', icon: CloudArrowDownIcon, current: false, count: 33 },
+  { name: 'Seeding', href: '#', icon: CloudArrowUpIcon, current: false, count: 89 },
+  { name: 'Error', href: '#', icon: ExclamationTriangleIcon, current: false, count: 0 },
 
 ]
 
 const trackers = [
 
-  {url: "https://tracker1.porla.org/announce/s0m3p455k3y", href: '#', icon: PorlaLogo, current: true, count: 2},
-  {url: "https://tracker2.porla.org/announce/4n0th3rp455k3y", href: '#', icon: PorlaLogo, current: false, count: 0}
+  { url: "https://tracker1.porla.org/announce/s0m3p455k3y", href: '#', icon: PorlaLogo, current: true, count: 2 },
+  { url: "https://tracker2.porla.org/announce/4n0th3rp455k3y", href: '#', icon: PorlaLogo, current: false, count: 0 }
 
 ]
 
@@ -114,90 +114,90 @@ function App() {
               <ul role="list" className="flex flex-1 flex-col gap-y-7">
                 <li>
                   <CollapsibleSection title="Torrents">
-                  <ul role="list" className="mt-2 space-y-1">
-                    {navigation.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? 'bg-gray-50 dark:bg-neutral-600 text-neutral-600 dark:text-neutral-200'
-                              : 'text-neutral-700 dark:text-neutral-500 hover:text-neutral-600 hover:dark:text-neutral-200 hover:bg-neutral-50 hover:dark:bg-neutral-700',
-                            'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                          )}
-                        >
-                          <item.icon
+                    <ul role="list" className="mt-2 space-y-1">
+                      {navigation.map((item) => (
+                        <li key={item.name}>
+                          <a
+                            href={item.href}
                             className={classNames(
                               item.current
-                                ? 'text-neutral-600 dark:text-neutral-200'
-                                : 'text-gray-400 dark:text-neutral-400 group-hover:text-neutral-600 group-hover:dark:text-neutral-200',
-                              'h-6 w-6 shrink-0'
+                                ? 'bg-gray-50 dark:bg-neutral-600 text-neutral-600 dark:text-neutral-200'
+                                : 'text-neutral-700 dark:text-neutral-500 hover:text-neutral-600 hover:dark:text-neutral-200 hover:bg-neutral-50 hover:dark:bg-neutral-700',
+                              'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                             )}
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                          {item.count >= 0 ? (
-                            <span
+                          >
+                            <item.icon
                               className={classNames(
                                 item.current
-                                  ? 'dark:ring-neutral-400'
-                                  : 'group-hover:dark:ring-neutral-500 dark:ring-neutral-600',
-                                'ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-white dark:bg-neutral-800 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-600 dark:text-neutral-400 ring-1 ring-inset ring-gray-200'
+                                  ? 'text-neutral-600 dark:text-neutral-200'
+                                  : 'text-gray-400 dark:text-neutral-400 group-hover:text-neutral-600 group-hover:dark:text-neutral-200',
+                                'h-6 w-6 shrink-0'
                               )}
                               aria-hidden="true"
-                            >
+                            />
+                            {item.name}
+                            {item.count >= 0 ? (
+                              <span
+                                className={classNames(
+                                  item.current
+                                    ? 'dark:ring-neutral-400'
+                                    : 'group-hover:dark:ring-neutral-500 dark:ring-neutral-600',
+                                  'ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-white dark:bg-neutral-800 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-600 dark:text-neutral-400 ring-1 ring-inset ring-gray-200'
+                                )}
+                                aria-hidden="true"
+                              >
                               {item.count}
                             </span>
-                          ) : null}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
+                            ) : null}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
                   </CollapsibleSection>
                 </li>
                 <li>
                   <CollapsibleSection title="Trackers">
-                  <ul role="list" className="mt-2 space-y-1">
-                    {trackers.map((tracker) => (
-                      <li key={tracker.url}>
-                        <a
-                          href={tracker.href}
-                          className={classNames(
-                            tracker.current
-                              ? 'bg-gray-50 dark:bg-neutral-600 text-neutral-600 dark:text-neutral-200'
-                              : 'text-neutral-700 dark:text-neutral-500 hover:text-neutral-600 hover:dark:text-neutral-200 hover:bg-neutral-50 hover:dark:bg-neutral-700',
-                            'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                          )}
-                        >
-                          <tracker.icon
+                    <ul role="list" className="mt-2 space-y-1">
+                      {trackers.map((tracker) => (
+                        <li key={tracker.url}>
+                          <a
+                            href={tracker.href}
                             className={classNames(
                               tracker.current
-                                ? 'text-neutral-600 dark:text-neutral-200'
-                                : 'text-gray-400 dark:text-neutral-400 group-hover:text-neutral-600 group-hover:dark:text-neutral-200',
-                              'h-6 w-6 shrink-0'
+                                ? 'bg-gray-50 dark:bg-neutral-600 text-neutral-600 dark:text-neutral-200'
+                                : 'text-neutral-700 dark:text-neutral-500 hover:text-neutral-600 hover:dark:text-neutral-200 hover:bg-neutral-50 hover:dark:bg-neutral-700',
+                              'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                             )}
-                            aria-hidden="true"
-                          />
-                          <div className="truncate">
-                            {(new URL(tracker.url)).hostname}
-                          </div>
-                          {tracker.count >= 0 ? (
-                            <span
+                          >
+                            <tracker.icon
                               className={classNames(
                                 tracker.current
-                                  ? 'dark:ring-neutral-400'
-                                  : 'group-hover:dark:ring-neutral-500 dark:ring-neutral-600',
-                                'ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-white dark:bg-neutral-800 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-600 dark:text-neutral-400 ring-1 ring-inset ring-gray-200'
+                                  ? 'text-neutral-600 dark:text-neutral-200'
+                                  : 'text-gray-400 dark:text-neutral-400 group-hover:text-neutral-600 group-hover:dark:text-neutral-200',
+                                'h-6 w-6 shrink-0'
                               )}
                               aria-hidden="true"
-                            >
+                            />
+                            <div className="truncate">
+                              {(new URL(tracker.url)).hostname}
+                            </div>
+                            {tracker.count >= 0 ? (
+                              <span
+                                className={classNames(
+                                  tracker.current
+                                    ? 'dark:ring-neutral-400'
+                                    : 'group-hover:dark:ring-neutral-500 dark:ring-neutral-600',
+                                  'ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-white dark:bg-neutral-800 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-600 dark:text-neutral-400 ring-1 ring-inset ring-gray-200'
+                                )}
+                                aria-hidden="true"
+                              >
                               {tracker.count}
                             </span>
-                          ) : null}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
+                            ) : null}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
                   </CollapsibleSection>
                 </li>
                 <li className="mt-auto">
