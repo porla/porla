893bd55c5cdccdd04e2a1657a82e3e0fa485ded8
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-12-18 23:29:02 +0100

    fix division by 0 and format progress percentage divisions cost more CPU than multiplications so i took a more complicated approach with multiplication

--- html/src/components/ProgressBar.tsx
+++ html/src/components/ProgressBar.tsx
@@ -15,7 +15,7 @@ type NonGradientProps = {
 type ProgressBarProps = GradientProps | NonGradientProps;
 
 export const ProgressBar = ({ progress, ...props }: ProgressBarProps) => {
-  const relativeFillPercentage = (100 / progress) * 100;
+  const relativeFillPercentage = 10000 * (progress ** -1);
   const { gradient = false, fromColor, toColor } = props;
 
   return (
@@ -28,7 +28,7 @@ export const ProgressBar = ({ progress, ...props }: ProgressBarProps) => {
           />
         </div>
       </div>
-      <div className="w-12 text-right">{`${progress}%`}</div>
+      <div className="w-12 text-right">{progress < 10 ? progress.toFixed(2) : (progress < 100 ? progress.toFixed(1) : progress.toFixed(0))}%</div>
     </div>
   );
 };
