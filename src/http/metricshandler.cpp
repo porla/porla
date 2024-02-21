738d0f38d82c85a032250c99a307d5a7d119248e
Dmitry Marchuk <code@dmarchuk.com>
2024-02-21 22:09:34 +0200

    Add per-torrent Prometheus metrics

--- src/http/metricshandler.cpp
+++ src/http/metricshandler.cpp
@@ -19,11 +19,13 @@ public:
         : m_stats(lt::session_stats_metrics())
     {
         m_stats_connection = sessions.OnSessionStats([this](auto && p1, auto && p2) { OnSessionStats(p1, p2); });
+        m_state_update_connection = sessions.OnStateUpdate([this](auto && p1, auto && p2) { OnStateUpdate(p1, p2); });
     }
 
     ~State()
     {
         m_stats_connection.disconnect();
+        m_state_update_connection.disconnect();
     }
 
     std::map<std::string, std::pair<std::uint64_t, lt::span<const int64_t>>>& SessionCounters()
@@ -34,6 +36,11 @@ public:
     std::vector<lt::stats_metric>& StatsMetrics()
     {
         return m_stats;
+    }
+
+    std::map<lt::sha1_hash, std::tuple<std::uint64_t, std::string, libtorrent::torrent_status>>& TorrentStatuses()
+    {
+        return m_torrent_statuses;
     }
 
 private:
@@ -49,8 +56,19 @@ private:
         m_session_counters.at(session) = { ms, stats};
     }
 
+    void OnStateUpdate(const std::string& session, const std::vector<libtorrent::torrent_status>& torrents)
+    {
+        for (const auto& status : torrents)
+        {
+            uint64_t ms = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+            m_torrent_statuses.insert_or_assign(status.info_hashes.v1, std::make_tuple(ms,session, status));
+        }
+    }
+
     boost::signals2::connection m_stats_connection;
+    boost::signals2::connection m_state_update_connection;
     std::map<std::string, std::pair<std::uint64_t, lt::span<const int64_t>>> m_session_counters;
+    std::map<lt::sha1_hash, std::tuple<std::uint64_t, std::string, libtorrent::torrent_status>> m_torrent_statuses;
     std::vector<lt::stats_metric> m_stats;
 };
 
@@ -64,6 +82,13 @@ MetricsHandler::~MetricsHandler() = default;
 void MetricsHandler::operator()(uWS::HttpResponse<false>* res, [[maybe_unused]] uWS::HttpRequest* req)
 {
     std::stringstream out;
+    this->PopulateWithGlobalMetrics(out);
+    this->PopulateWithPerTorrentMetrics(out);
+
+    res->writeStatus("200 OK")->end(out.str());
+}
+
+void MetricsHandler::PopulateWithGlobalMetrics(std::stringstream& out) {
 
     for (const auto& metric : m_state->StatsMetrics())
     {
@@ -104,6 +129,61 @@ void MetricsHandler::operator()(uWS::HttpResponse<false>* res, [[maybe_unused]] 
 
         out << "\n";
     }
+}
 
-    res->writeStatus("200 OK")->end(out.str());
+void MetricsHandler::PopulateWithPerTorrentMetrics(std::stringstream &out) {
+    for (const auto &[info_hash, tuple]: m_state->TorrentStatuses()) {
+        const auto [ms, session, status] = tuple;
+        auto makeCounter = [&out, &session, &ms](
+                const char *name,
+                const std::int64_t value
+        ) {
+            out << "# TYPE porla_torrent_status_" << name << " counter\n";
+            out << "porla_torrent_status_" << name << "{session=\"" << session << "\", name=\"" << name << "\"} " << value
+                << " " << ms << "\n\n";
+        };
+        makeCounter("total_download", status.total_download);
+        makeCounter("total_upload", status.total_upload);
+        makeCounter("total_payload_download", status.total_payload_download);
+        makeCounter("total_payload_upload", status.total_payload_upload);
+        makeCounter("total_failed_bytes", status.total_failed_bytes);
+        makeCounter("total_redundant_bytes", status.total_redundant_bytes);
+        makeCounter("total_done", status.total_done);
+        makeCounter("total", status.total);
+        makeCounter("total_wanted_done", status.total_wanted_done);
+        makeCounter("total_wanted", status.total_wanted);
+        makeCounter("all_time_upload", status.all_time_upload);
+        makeCounter("all_time_download", status.all_time_download);
+        makeCounter("added_time", status.added_time);
+        makeCounter("completed_time", status.completed_time);
+        makeCounter("last_seen_complete", status.last_seen_complete);
+        makeCounter("progress_ppm",  status.progress_ppm);
+        makeCounter("queue_position", status.queue_position.operator int());
+        makeCounter("download_rate", status.download_rate);
+        makeCounter("upload_rate", status.upload_rate);
+        makeCounter("download_payload_rate", status.download_payload_rate);
+        makeCounter("upload_payload_rate", status.upload_payload_rate);
+        makeCounter("num_seeds", status.num_seeds);
+        makeCounter("num_peers", status.num_peers);
+        makeCounter("num_complete", status.num_complete);
+        makeCounter("num_incomplete", status.num_incomplete);
+        makeCounter("list_seeds", status.list_seeds);
+        makeCounter("list_peers", status.list_peers);
+        makeCounter("connect_candidates", status.connect_candidates);
+        makeCounter("num_pieces", status.num_pieces);
+        makeCounter("distributed_full_copies", status.distributed_full_copies);
+        makeCounter("distributed_fraction", status.distributed_fraction);
+        makeCounter("block_size", status.block_size);
+        makeCounter("num_uploads", status.num_uploads);
+        makeCounter("num_connections", status.num_connections);
+        makeCounter("uploads_limit", status.uploads_limit);
+        makeCounter("connections_limit", status.connections_limit);
+        makeCounter("up_bandwidth_queue", status.up_bandwidth_queue);
+        makeCounter("down_bandwidth_queue", status.down_bandwidth_queue);
+        makeCounter("seed_rank", status.seed_rank);
+        makeCounter("state", status.state);
+        makeCounter("active_duration", status.active_duration.count());
+        makeCounter("finished_duration", status.finished_duration.count());
+        makeCounter("seeding_duration", status.seeding_duration.count());
+    }
 }
