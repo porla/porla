be3f558a8c7ecc72787afb63ea40c7c620325fba
martylukyy <35452459+martylukyy@users.noreply.github.com>
2024-05-04 23:01:23 +0200

    chore: code cleanup

--- html/src/main.tsx
+++ html/src/main.tsx
@@ -1,15 +1,3 @@
-/*
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-*/
-
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 
