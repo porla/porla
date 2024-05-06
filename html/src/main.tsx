c82134f3f62b23aaba041a826a13f5f9cb1743e0
martylukyy <35452459+martylukyy@users.noreply.github.com>
2024-05-06 19:22:58 +0200

    feat(web): add strict mode again

--- html/src/main.tsx
+++ html/src/main.tsx
@@ -2,10 +2,15 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import { App } from "./App.tsx";
+import React from 'react';
 
 const rootElement = document.getElementById('root')!
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(<App />)
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 }
