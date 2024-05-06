7c4c35529d4cc608d074952eb6718d53232967cb
martylukyy <35452459+martylukyy@users.noreply.github.com>
2024-05-06 12:07:40 +0200

    fix(web): imports

--- html/src/App.tsx
+++ html/src/App.tsx
@@ -1,5 +1,22 @@
 import { AuthProvider, useAuth } from "./auth.tsx";
-import { RouterProvider } from "@tanstack/react-router";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
+
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  context: {
+    auth: undefined!, // This will be set after we wrap the app in an AuthProvider
+  },
+})
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 function InnerApp() {
   const auth = useAuth()
--- html/src/main.tsx
+++ html/src/main.tsx
@@ -1,26 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { createRouter } from '@tanstack/react-router'
-
-import { routeTree } from './routeTree.gen'
 
 import './index.css'
 import { App } from "./App.tsx";
-
-// Set up a Router instance
-const router = createRouter({
-  routeTree,
-  defaultPreload: 'intent',
-  context: {
-    auth: undefined!, // This will be set after we wrap the app in an AuthProvider
-  },
-})
-
-// Register things for typesafety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
 
 const rootElement = document.getElementById('root')!
 
