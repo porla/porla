2e1c40670279a1ca5cd3aac218dca6b33e55bcd6
Viktor Elofsson <viktor@viktorelofsson.se>
2024-12-02 17:37:52 +0100

    fix gitversion, add some more version info

--- cmake/GenerateVersion.cmake
+++ cmake/GenerateVersion.cmake
@@ -17,13 +17,35 @@ if ("${GIT_COMMITISH}" STREQUAL "")
 endif()
 
 set(VERSION "$ENV{GITVERSION_SEMVER}")
+set(VERSION_MAJOR "$ENV{GITVERSION_MAJOR}")
+set(VERSION_MINOR "$ENV{GITVERSION_MINOR}")
+set(VERSION_PATCH "$ENV{GITVERSION_PATCH}")
+
+if ("${VERSION_MAJOR}" STREQUAL "")
+    set(VERSION_MAJOR "0")
+endif()
+
+if ("${VERSION_MINOR}" STREQUAL "")
+    set(VERSION_MINOR "0")
+endif()
+
+if ("${VERSION_PATCH}" STREQUAL "")
+    set(VERSION_PATCH "0")
+endif()
 
 string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
 string(STRIP "${GIT_COMMITISH}" GIT_COMMITISH)
 
-set(VERSION_SRC "const char* GIT_BRANCH=\"${GIT_BRANCH}\";
+set(VERSION_SRC "
+const char* GIT_BRANCH=\"${GIT_BRANCH}\";
 const char* GIT_COMMITISH=\"${GIT_COMMITISH}\";
-const char* VERSION=\"${VERSION}\";")
+const char* VERSION=\"${VERSION}\";
+const char* VERSION_MAJOR=\"${VERSION_MAJOR}\";
+const char* VERSION_MINOR=\"${VERSION_MINOR}\";
+const char* VERSION_PATCH=\"${VERSION_PATCH}\";
+")
+
+message(VERSION_SRC="${VERSION_SRC}")
 
 if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/version.cpp)
     file(READ ${CMAKE_CURRENT_SOURCE_DIR}/version.cpp VERSION_SRC_)
--- src/buildinfo.cpp
+++ src/buildinfo.cpp
@@ -1,5 +1,7 @@
 #include "buildinfo.hpp"
 
+#include <string>
+
 using porla::BuildInfo;
 
 extern "C"
@@ -7,6 +9,9 @@ extern "C"
     extern const char* GIT_BRANCH;
     extern const char* GIT_COMMITISH;
     extern const char* VERSION;
+    extern const char* VERSION_MAJOR;
+    extern const char* VERSION_MINOR;
+    extern const char* VERSION_PATCH;
 }
 
 const char* BuildInfo::Branch()
@@ -22,4 +27,19 @@ const char* BuildInfo::Commitish()
 const char* BuildInfo::Version()
 {
     return VERSION;
+}
+
+const int BuildInfo::VersionMajor()
+{
+    return atoi(VERSION_MAJOR);
+}
+
+const int BuildInfo::VersionMinor()
+{
+    return atoi(VERSION_MINOR);
+}
+
+const int BuildInfo::VersionPatch()
+{
+    return atoi(VERSION_PATCH);
 }
