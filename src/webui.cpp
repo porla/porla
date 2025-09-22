06b7c8983f3c8b0a566d9eb469bc53e511cf69ba
Viktor Elofsson <viktor@viktorelofsson.se>
2025-09-22 20:09:06 +0200

    fetch webui asset data

--- src/webui.cpp
+++ src/webui.cpp
@@ -1,5 +1,6 @@
 #include "webui.hpp"
 
+#include <fstream>
 #include <sstream>
 
 #include <boost/log/trivial.hpp>
@@ -11,6 +12,12 @@
 using json = nlohmann::json;
 using porla::WebUI;
 
+struct HttpResponse
+{
+    std::string body;
+    long status_code;
+};
+
 static size_t HttpWriteCallback(char *ptr, size_t size, size_t nmemb, void *userdata)
 {
     std::stringstream* ss = reinterpret_cast<std::stringstream*>(userdata);
@@ -18,29 +25,83 @@ static size_t HttpWriteCallback(char *ptr, size_t size, size_t nmemb, void *user
     return nmemb;
 }
 
+static HttpResponse HttpGet(const std::string& url)
+{
+    std::stringstream user_agent;
+    user_agent << "porla/" << porla::BuildInfo::Version();
+
+    std::stringstream http_response_body;
+
+    CURL* curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &http_response_body);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, HttpWriteCallback);
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, user_agent.str().c_str());
+
+    if (curl_easy_perform(curl) != CURLE_OK)
+    {
+        curl_easy_cleanup(curl);
+        return HttpResponse{
+            .body        = {},
+            .status_code = -1
+        };
+    }
+
+    long response_code;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+
+    curl_easy_cleanup(curl);
+
+    return HttpResponse{
+        .body        = http_response_body.str(),
+        .status_code = response_code
+    };
+}
+
 void WebUI::Download(const std::string& repo, const fs::path& target_file)
 {
     std::stringstream url;
     url << "https://api.github.com/repos/" << repo << "/releases/latest";
 
-    std::stringstream user_agent;
-    user_agent << "porla/" << BuildInfo::Version();
-
     BOOST_LOG_TRIVIAL(info) << "Downloading web UI from github.com/" << repo << " to " << target_file;
 
-    std::stringstream http_response_body;
+    HttpResponse latest_release = HttpGet(url.str());
 
-    CURL* curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &http_response_body);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, HttpWriteCallback);
-    curl_easy_setopt(curl, CURLOPT_URL, url.str().c_str());
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, user_agent.str().c_str());
-    curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
+    if (latest_release.status_code != 200)
+    {
+        BOOST_LOG_TRIVIAL(error) << "Failed to fetch latest web UI release: " << latest_release.body;
+        return;
+    }
 
-    json release = json::parse(http_response_body.str());
+    json release;
+
+    try
+    {
+        release = json::parse(latest_release.body);
+    }
+    catch (const std::exception& ex)
+    {
+        BOOST_LOG_TRIVIAL(error) << "Failed to parse release data: " << ex.what();
+        return;
+    }
+
     std::string tag_name = release["tag_name"];
     std::string download_url = release["assets"][0]["browser_download_url"];
 
     BOOST_LOG_TRIVIAL(info) << "Found version " << tag_name << " of web UI - fetching from " << download_url;
+
+    HttpResponse asset_data = HttpGet(download_url);
+
+    if (asset_data.status_code != 200)
+    {
+        BOOST_LOG_TRIVIAL(error) << "Failed to fetch web UI asset data: " << asset_data.body;
+        return;
+    }
+
+    BOOST_LOG_TRIVIAL(info) << "Downloaded " << asset_data.body.size() << " bytes of fresh web UI";
+
+    std::ofstream asset_output_file(target_file, std::ios::binary);
+    asset_output_file << asset_data.body;
+    asset_output_file.flush();
 }
