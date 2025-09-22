e3376ec7ad43265789ae53d0ff1e9635c966475f
Viktor Elofsson <viktor@viktorelofsson.se>
2025-09-22 17:34:12 +0200

    debundle webui

--- html/src/base.ts
+++
@@ -1,5 +0,0 @@
-export default function prefixPath(path: string) {
-  let base = (window as any).porla.base_path as string;
-  if (base.endsWith("/")) base = base.substring(0, base.length - 1);
-  return `${base}${path}`;
-}
