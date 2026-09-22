cffc9fb997d1850738e08075946c3b9c74e83e5f
Viktor Elofsson <viktor@viktorelofsson.se>
2026-09-22 22:09:16 +0200

    move this_state in property

--- types/lua/usertypes/lttorrenthandle.lua
+++ types/lua/usertypes/lttorrenthandle.lua
@@ -2,3 +2,6 @@
 
 ---@class LtTorrentHandle
 local LtTorrentHandle = {}
+
+---@return LtInfoHash
+function LtTorrentHandle:info_hash() end
--- types/lua/usertypes/lttorrentstatus.lua
+++ types/lua/usertypes/lttorrentstatus.lua
@@ -5,11 +5,15 @@
 ---@field all_time_download integer
 ---@field all_time_upload integer
 ---@field download_payload_rate integer
+---@field errc PoError?
+---@field flags LtTorrentFlags
 ---@field info_hash LtInfoHash
+---@field moving_storage boolean
 ---@field name string
 ---@field progress number
 ---@field save_path string
 ---@field seeding_duration integer
+---@field state string?
 ---@field total_wanted integer
 ---@field total_wanted_done integer
 ---@field upload_payload_rate integer
